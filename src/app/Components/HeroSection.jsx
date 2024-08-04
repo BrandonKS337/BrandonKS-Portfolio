8a8b78f1e96ca3ce4fd8f11d23282b5a4d5a3227
Your Name <brandonks337@gmail.com>
2024-08-03 21:40:03 -0500

    Hero Section Setup and ready to go. Need a good Professional Photo SVG to replace placeholder but set variable up to allow for quick swap.

--- src/app/Components/HeroSection.jsx
+++ src/app/Components/HeroSection.jsx
@@ -1,5 +1,9 @@
 "use client";
 import React from "react";
+import { HERO_SECTION } from "../constants";
+import Image from "next/image";
+
+const profilePic = "/assets/placeholderimages/placeholderProfile.jpg";
 
 const HeroSection = () => {
   return (
@@ -7,22 +11,26 @@ const HeroSection = () => {
       <div className="flex flex-wrap">
         <div className="w-full lg:w-1/2 ">
           <div className="flex flex-col items-center lg:items-start">
-                  {/* Intro */}
-                  <div className="text-white">
-                    <h2 className="pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl">Brandon KennedySmith</h2>
-                    <p className="bg-gradient-to-r from-blue-300 via bg-slate-500 to-yellow-400 bg-clip-text text-3xl tracking-tight text-transparent">Full Stack Software Engineer</p>
-                    <p className="my-2">Placeholder Text</p>
-                  </div>
-                  {/* Hero Image */}
-                  <div>
-                    <img
-                      src="/assets/placeholderimages/placeholderProfile.jpg"
-                      alt="Profile Hero Image"
-                    />
-                  </div>
-                </div>
-            </div>
+            {/* Intro */}
+            <h2 className="pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-7xl ">
+              Brandon KennedySmith
+            </h2>
+            <p className="bg-gradient-to-r from-blue-300 via bg-slate-500 to-yellow-400 bg-clip-text text-3xl tracking-tight text-transparent">
+              Full Stack Software Engineer
+            </p>
+            <p className="my-2 max-w-xl py-6 font-light tracking-tighter">
+              {HERO_SECTION}
+            </p>
+            {/* <p className="my-2">Placeholder</p> */}
+          </div>
         </div>
+        {/* Hero Image */}
+        <div className="w-full lg:w-1/2 lg:p-4 ">
+          <div className="flex justify-center ">
+            <img src={profilePic} alt="Profile Hero Image" />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
---
+++ src/app/constants/index.js
@@ -0,0 +1,73 @@
+export const HERO_SECTION =
+  " I am a passionate full stack developer with a knack fro crafting robust and scalable applications. With exposure to a wide array of technologies, I have honed my skills and expanded my exposure in everything from frontend to backend technologies like Next.js, Express & MySQL. My goal is to take all my experiences and leverage those to create innovative solutions that drive business growth and deliver exceptional user experience.";
+
+export const ABOUT_ME_TEXT =
+  "I am a dedicated and versatile full stack software engineer with a drive for creating unique, efficient and user-friendly applications. I have worked with a variety of technologies like React, Next.JS, MYSQL, MongoDB, AWS EC2 to name a few. My Journey in Software Engineering began with a deep curiosity for how the internet works, and it has evolved into a wonderful expedition  where I continuously strive to learn and adapt to new challenges.";
+
+export const EXPERIENCES = [
+  {
+    year: "2024 - Present",
+    role: "Full Stack Software Engineer",
+    company: "Quantum Sky",
+    description:
+      "Small startup focusing on Engineering the future through innovative software solutions. Collaborating closely with a dedicated team, we take concepts from inception to launch, crafting applications that not only inspire but also provide an immersive experience. Our commitment to creating an inclusive environment ensures that everyone feels welcome to return, time and time again. By adhering to the highest standards of coding excellence, we incorporate best practices and a user-centric design philosophy, driving us towards our mission of making technology accessible and enjoyable for all. Our journey is marked by a relentless pursuit of innovation, dedication to quality, and a passion for exceeding user expectations.",
+    technologies: [
+      "React",
+      "Next.JS",
+      "Figma",
+      "Trello",
+      "MongoDB",
+      "Express",
+      "Postgres",
+      "GitBash",
+      "VSCode",
+      "Agile/Scrum",
+    ],
+  },
+  {
+    year: "2023-2024",
+    role: "Full Stack Software Engineer - Instructor",
+    company: "Institute of Data",
+    description:
+      "Serve as an integral part of the teaching team, enhancing student learning experiences in a fast-paced software development course. Demonstrate expertise by swiftly addressing coding errors during breakout sessions, ensuring uninterrupted main lecture flow. Spearhead troubleshooting for student project glitches, crafted coding examples to illuminate specific exercises, and meticulously document each session for continuous course improvement and to aid students with revisiting recorded course material. Track and assist with grading individual assignments making sure they are up to current code standards and structure and update class progression on several different platforms such as slack, google sheets and Monday.com. Collaboratively work with the lead instructor, merging academic rigor with practical insights to deliver a dynamic and impactful curriculum.",
+    technologies: [
+      "React",
+      "Next.JS",
+      "Figma",
+      "Trello",
+      "MongoDB",
+      "Express",
+      "Postgres",
+      "GitBash",
+      "VSCode",
+      "Agile/Scrum",
+      "SQL/MySQL",
+      "Postman",
+      "AWS",
+      "MaterialUI",
+      "API Development",
+      "Docker",
+      "Monday",
+      "Zoom",
+    ],
+  },
+  {
+    year: "2015 - Present",
+    role: "Field Supervisor / Senior Technical Instructor",
+    company: "TNT Cable Contractors",
+    description:
+      "As a Senior Technician/Field Supervisor at TNT Cable Contractors, I oversee field operations in partnership with Cox Communications, ensuring the efficient execution of projects. My responsibilities include assigning tasks to field technicians based on their skills and availability, and monitoring daily workflow to ensure timely project completion. I conduct in-depth analysis of production issues, implement effective solutions to improve technical procedures, and provide comprehensive training on equipment operation, safety protocols, and company policies. Additionally, I facilitate communication across multiple teams and external partners, simplifying complex technical information to ensure clarity and effective collaboration. My role is pivotal in maintaining high standards of safety, quality, and productivity within our operations.",
+    technologies: [
+      "Teamwork",
+      "Multi-tasking",
+      "Interpersonal Communication",
+      "Project Management",
+      "Documentation",
+      "Presentation Skills",
+      "Operating Systems",
+      "Creative Problem Solving",
+      "Agile Project Management",
+      "Teams",
+    ],
+  },
+];
