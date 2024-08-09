920017ffca703eb52452088aacefb750c02565ad
Project V2 <brandonks337@gmail.com>
2024-08-08 23:37:58 -0500

    Pushing up changes for the Tech Stack section and some minor tailwind modifications. Going to try to build in a carousel to showcase the technologies section but may need to wait on that for now because of the difficulty.

--- src/app/Components/AboutSection.jsx
+++ src/app/Components/AboutSection.jsx
@@ -5,22 +5,24 @@ const aboutMeImg = "/assets/placeholderimages/placeholderProfile.jpg";
 
 const AboutSection = () => {
   return (
-    <div className="border-b border-neutral-900 pb-4">
-      <h2 className="my-20 text-center text-4xl">
-        About
-        <span className="text-neutral-700"> Me</span>
-      </h2>
-      <div className="flex flex-wrap ">
-        <div className="flex items-center w-full lg:w-1/2 lg:p-8 justify-centers">
-          <img
-            src={aboutMeImg}
-            alt="Profile Image 2"
-            className=" rounded-2xl"
-          />
-        </div>
-        <div className="flex justify-center w-full lg:justify-start lg:w-1/2">
-          <p className="my-2 max-w-xl py-6">{ABOUT_ME_TEXT}</p>
-        </div>
+    <div className="border-b border-neutral-700 pb-20 lg:mb-35">
+      
+        <h2 className="my-20 text-center text-4xl">
+          About
+          <span className="text-neutral-700"> Me</span>
+        </h2>
+        <div className="flex flex-wrap ">
+          <div className="flex items-center w-full lg:w-1/2 lg:p-8 justify-center">
+            <img
+              src={aboutMeImg}
+              alt="Profile Image 2"
+              className=" rounded-2xl"
+            />
+          </div>
+          <div className="flex justify-center w-full lg:justify-start lg:w-1/2">
+            <p className="my-2 max-w-xl py-6">{ABOUT_ME_TEXT}</p>
+          </div>
+        
       </div>
     </div>
   );
--- src/app/Components/TechStack.jsx
+++ src/app/Components/TechStack.jsx
@@ -1,9 +1,99 @@
-import React from 'react'
+import React from "react";
+import { RiReactjsLine } from "react-icons/ri";
+import { TbBrandNextjs } from "react-icons/tb";
+import { SiMysql } from "react-icons/si";
+import { FaGithub } from "react-icons/fa";
+import { SiMui } from "react-icons/si";
+import { RiTailwindCssFill } from "react-icons/ri";
+import { FaNodeJs } from "react-icons/fa";
+import { SiMongodb } from "react-icons/si";
+// import { BsFiletypeSql } from "react-icons/bs";
+import { SiSequelize } from "react-icons/si";
+import { SiAmazonec2 } from "react-icons/si";
+import { FaAws } from "react-icons/fa";
+import { FaDocker } from "react-icons/fa";
+import { PiFigmaLogoFill } from "react-icons/pi";
+import { FaTrello } from "react-icons/fa";
+import { CgMonday } from "react-icons/cg";
+import { VscVscodeInsiders } from "react-icons/vsc";
+import { SiPostman } from "react-icons/si";
+import { SiSentry } from "react-icons/si";
+import { FaSlack } from "react-icons/fa";
+import { SiVercel } from "react-icons/si";
 
 const TechStack = () => {
   return (
-    <div>TechStack</div>
-  )
-}
+    <div className="border-b border-neutral-800 pb-24">
+      <h2 className="mt-20 text-center text-4xl">Technologies</h2>
+      <p className="text-center m-20 px-15">
+        Here are a few of the technologies and resources I have familiarized
+        myself with through direct project based experience
+      </p>
+      <div className="flex flex-wrap items-center justify-center gap-4">
+        <div className="rounded-2xl border-4 border-neutral-800 p-4">
+          <RiReactjsLine className="text-7xl text-cyan-400" />
+        </div>
+        <div className="rounded-2xl border-4 border-neutral-800 p-4">
+          <TbBrandNextjs className="text-7xl" />
+        </div>
+        <div className="rounded-2xl border-4 border-neutral-800 p-4">
+          <FaDocker className="text-7xl text-blue-700" />
+        </div>
+        <div className="rounded-2xl border-4 border-neutral-800 p-4">
+          <FaGithub className="text-7xl" />
+        </div>
+        <div className="rounded-2xl border-4 border-neutral-800 p-4">
+          <SiMongodb className="text-7xl text-green-500" />
+        </div>
+        <div className="rounded-2xl border-4 border-neutral-800 p-4">
+          <SiMysql className="text-7xl text-cyan-400" />
+        </div>
+        <div className="rounded-2xl border-4 border-neutral-800 p-4">
+          <SiSequelize className="text-7xl text-cyan-400" />
+        </div>
+        <div className="rounded-2xl border-4 border-neutral-800 p-4">
+          <CgMonday className="text-7xl text-red-500" />
+        </div>
+        <div className="rounded-2xl border-4 border-neutral-800 p-4">
+          <FaTrello className="text-7xl text-cyan-500" />
+        </div>
+        <div className="rounded-2xl border-4 border-neutral-800 p-4">
+          <PiFigmaLogoFill className="text-7xl" />
+        </div>
+        <div className="rounded-2xl border-4 border-neutral-800 p-4">
+          <SiMui className="text-7xl text-cyan-400" />
+        </div>
+        <div className="rounded-2xl border-4 border-neutral-800 p-4">
+          <RiTailwindCssFill className="text-7xl text-cyan-400" />
+        </div>
+        <div className="rounded-2xl border-4 border-neutral-800 p-4">
+          <FaNodeJs className="text-7xl text-green-500" />
+        </div>
+        {/* <div className="rounded-2xl border-4 border-neutral-800 p-4"><BsFiletypeSql className="text-7xl text-cyan-400"/></div> */}
+        <div className="rounded-2xl border-4 border-neutral-800 p-4">
+          <FaAws className=" text-7xl text-blue-600" />
+        </div>
+        <div className="rounded-2xl border-4 border-neutral-800 p-4">
+          <SiAmazonec2 className="text-7xl text-orange-500" />
+        </div>
+        <div className="rounded-2xl border-4 border-neutral-800 p-4">
+          <VscVscodeInsiders className="text-7xl text-cyan-500" />
+        </div>
+        <div className="rounded-2xl border-4 border-neutral-800 p-4">
+          <SiPostman className="text-7xl text-orange-500 bg-white rounded-2xl" />
+        </div>
+        <div className="rounded-2xl border-4 border-neutral-800 p-4">
+          <SiSentry className="text-7xl text-red-500" />
+        </div>
+        <div className="rounded-2xl border-4 border-neutral-800 p-4">
+          <FaSlack className="text-7xl" />
+        </div>
+        <div className="rounded-2xl border-4 border-neutral-800 p-4">
+          <SiVercel className="text-7xl" />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default TechStack
+export default TechStack;
